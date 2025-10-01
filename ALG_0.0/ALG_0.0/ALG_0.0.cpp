1ff03c9a469bb496559f84dc1ffddff465734801
“Aleksey <aleksey.kudryavtsew@gmail.com>
2025-10-01 18:13:14 +0300

    alg0,1

---
+++ ALG_0.0/ALG_0.0/ALG_0.0.cpp
@@ -0,0 +1,28 @@
+﻿#include <algorithm>
+#include <fstream>
+#include <vector>
+
+int main()
+{
+	std::ifstream in("input.txt");
+	std::ofstream out("output.txt");
+
+	std::vector<int> vec;
+	int num;
+	while (in >> num) {
+		vec.push_back(num);
+	
+	}
+	std::sort(vec.begin(), vec.end());
+	auto endv = std::unique(vec.begin(), vec.end());
+	vec.erase(endv, vec.end());
+
+	long long sum = 0;
+	for (int i = 0; i < vec.size(); i++) {
+		sum += vec[i];
+	}
+	out << sum;
+	out.close();
+	in.close();
+
+}
---
+++ ALG_0.1_BS/ALG_0.1_BS/ALG_0.1_BS.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+bool BinSearch(int* arr, int n, int x) {
+	int q = 0, k;
+
+	while (q < n) {
+		k = (q + n) / 2;
+		if (x == arr[k]) return 1;
+
+		else if (x < arr[k]) n = k;
+
+		else q = k + 1;
+	}
+	return 0;
+}
+
+int LowerBound(int* arr, int n, int x) {
+	int q = 0, k;
+
+	while (q < n) {
+		k = (q + n) / 2;
+		if (x <= arr[k]) n = k;
+
+		else q = k + 1;
+	}
+	return q;
+}
+
+int UpperBound(int* arr, int n, int x) {
+	int q = 0, k;
+
+	while (q < n) {
+		k = (q + n) / 2;
+		if (x < arr[k]) n = k;
+
+		else q = k + 1;
+	}
+	return q;
+}
+
+
+int main() {
+	std::iostream::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
+
+	int n, value, k;
+	std::cin >> n;
+	int* arr = new int[n];
+	
+	for (int i = 0; i < n; i++) {
+		std::cin >> arr[i];
+	}
+
+	std::cin >> k;
+	for (int j = 0; j < k; j++) {
+		std::cin >> value;
+		std::cout << BinSearch(arr, n, value) << " ";
+		std::cout << LowerBound(arr, n, value) << " ";
+		std::cout << UpperBound(arr, n, value) << " " << "\n";
+
+	}
+	delete[] arr;
+}

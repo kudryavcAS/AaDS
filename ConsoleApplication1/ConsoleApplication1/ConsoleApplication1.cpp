da1570ae2c64fa60848557dace44e6abe5832f2d
“Aleksey <aleksey.kudryavtsew@gmail.com>
2025-10-11 17:19:42 +0300

    7LIS+readme

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,10 @@
+﻿int main() {
+
+	int* arr = new int[5];
+
+	int x = arr[5];
+
+
+
+
+}
---
+++ Rec_7_LIS/Rec_7_LIS/Rec_7_LIS.cpp
@@ -0,0 +1,46 @@
+﻿#include <fstream>
+#include <vector>
+
+int LowerBound(const std::vector<int>& vec, int x) {
+	int q = 0;
+	int n = vec.size();
+
+	while (q < n) {
+		int k = (q + n) / 2;
+		if (x <= vec[k]) {
+			n = k;
+		}
+		else {
+			q = k + 1;
+		}
+	}
+	return q;
+}
+
+int main() {
+	
+	std::ifstream in("input.txt");
+	std::ofstream out("output.txt");
+
+	int n, len;
+	in >> n;
+	
+	std::vector<int> seq(n), last;
+	for (int i = 0; i < n; i++) {
+		in >> seq[i];
+	}
+
+	for (int x : seq) {
+		int pos = LowerBound(last, x);
+		
+		if (pos == last.size()) {
+			last.push_back(x);
+		}
+		else {
+			last[pos] = x;
+		}
+	}
+
+	out << last.size();
+
+}

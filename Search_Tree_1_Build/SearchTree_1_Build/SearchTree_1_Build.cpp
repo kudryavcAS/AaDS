66248d7a33d20681748591a78ed6ac802ed5e342
“Aleksey <aleksey.kudryavtsew@gmail.com>
2025-10-24 20:53:16 +0300

    bin_serach_tree

---
+++ Search_Tree_1_Build/SearchTree_1_Build/SearchTree_1_Build.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+template <typename T>
+class SearchBinTree {
+private:
+	struct Vertex {
+		T value;
+		Vertex* left = nullptr;
+		Vertex* right = nullptr;
+
+		Vertex(int x) {
+			value = x;
+		}
+
+		void printDirectLeftBypass(std::ofstream& out) {
+			out << this->value << "\n";
+
+			if (this->left != nullptr) {
+				this->left->printDirectLeftBypass(out);
+			}
+			if (this->right != nullptr) {
+				this->right->printDirectLeftBypass(out);
+			}
+		}
+	};
+
+	Vertex* root = nullptr;
+	int count = 0;
+
+public:
+	SearchBinTree(int count) {
+		this->count = count;
+	}
+	SearchBinTree() {}
+
+	Vertex* getRoot() {
+		return root;
+	}
+
+	void SetVertex(int x) {
+		if (root == nullptr) {
+			root = new Vertex(x);
+			count++;
+			return;
+		}
+		Vertex* current = root;
+		while (current != nullptr) {
+			if (x > current->value) {
+				if (current->right != nullptr)
+				{
+					current = current->right;
+				}
+				else {
+					current->right = new Vertex(x);
+					count++;
+					break;
+				}
+			}
+			else if (x < current->value) {
+				if (current->left != nullptr)
+				{
+					current = current->left;
+				}
+				else {
+					current->left = new Vertex(x);
+					count++;
+					break;
+				}
+			}
+			else {
+				break;
+			}
+		}
+	}
+
+	std::vector<T>& directLeftBypass(std::vector<T> vec) {
+
+	}
+
+	void printDirectLeftBypass(std::ofstream& out) {
+		if (root != nullptr) {
+			root->printDirectLeftBypass(out);
+		}
+	}
+};
+
+int main() {
+	std::ifstream in("input.txt");
+	std::ofstream out("output.txt");
+
+	int value;
+	SearchBinTree<int> tree;
+
+	while (in >> value) {
+		tree.SetVertex(value);
+	}
+	in.close();
+
+	tree.printDirectLeftBypass(out);
+	out.close();
+
+}
---
+++ Search_Tree_3_Check/Search_Tree_3_Check/Search_Tree_3_Check.cpp
@@ -0,0 +1,56 @@
+﻿#include <vector>
+#include <fstream>
+#include <cstdio>
+
+using namespace std;
+int main() {
+
+	FILE* in = fopen("bst.in", "r");
+	FILE* out = fopen("bst.out", "w");
+
+	int n, key, parent;
+	char side;
+	fscanf(in, "%d", &n);
+
+	std::vector<int>keys(n + 1);
+	std::vector<int>leftBound(n + 1);
+	std::vector<int>rightBound(n + 1);
+
+	fscanf(in, "%d", &key);
+	keys[1] = key;
+	leftBound[1] = INT_MIN;
+	rightBound[1] = INT_MAX;
+
+
+	for (int i = 2; i < n + 1; i++) {
+		fscanf(in, "%d %d %c", &key, &parent, &side);
+
+		if (side == 'L') {
+			if (key < leftBound[parent] || key >= keys[parent]) {
+				fprintf(out, "NO");
+				fclose(in);
+				fclose(out);
+				return 0;
+			}
+			keys[i] = key;
+			leftBound[i] = leftBound[parent];
+			rightBound[i] = keys[parent] - 1;
+		}
+		else {
+			if (key < keys[parent] || key > rightBound[parent]) {
+				fprintf(out, "NO");
+				fclose(in);
+				fclose(out);
+				return 0;
+			}
+			keys[i] = key;
+			leftBound[i] = keys[parent];
+			rightBound[i] = rightBound[parent];
+		}
+
+	}
+
+	fprintf(out, "YES");
+	fclose(in);
+	fclose(out);
+}
